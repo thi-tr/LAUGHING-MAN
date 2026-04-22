29d208802a2e663b0fb4d57b0f118e870f09c46d
thi-tr <thi.t2103@gmail.com>
2026-04-22 10:50:48 -0400

    Squares can now bounce on the sides

--- laughing_man.js
+++ laughing_man.js
@@ -1,4 +1,4 @@
-const SQUARE_COUNT = 30;
+const SQUARE_COUNT = 100;
 const TIMER_SPEED = 16.6;
 const SPEED = 5;
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -18,12 +18,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
         box.appendChild(square);
     }
    Array.from(box.children).forEach((element)=>{
-        let dx = SPEED * Math.random()*2-1; //This gives a number from -1 to 0.999999
-        let dy = SPEED * Math.random()*2-1;
+        const parent = element.parentElement;
+        const maxX = parent.clientWidth - element.clientWidth;
+        const maxY = parent.clientHeight - element.clientHeight;
 
-        let x = parseInt(element.style.left) || 0;
-        let y = parseInt(element.style.top) || 0;
+        let dx = SPEED * (Math.random()*2-1); //This gives a number from -1 to 0.999999
+        let dy = SPEED * (Math.random()*2-1);
+
+        let x = parseInt(element.style.left) || 225;
+        let y = parseInt(element.style.top) || 175;
         setInterval(()=>{
+
+            if(x <= 0 || x >= maxX){
+                dx *= -1;
+            }
+            if(y <= 0 || y >= maxY){
+                dy *= -1;
+            }
+
             x += dx;
             y += dy;
 
